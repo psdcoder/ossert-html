506810424e7a76ced3263d81bc3395f4105e3589
Pavel Grinchenko <psdcoder@gmail.com>
2016-09-09 00:58:58 +0300

    Start base layout

--- postcss/css-vars.js
+++ postcss/css-vars.js
@@ -1,7 +1,17 @@
 module.exports = {
-    layoutBg: '#ccc',
-    layoutBorder: '#888',
-    layoutBorderRadius: '4px',
+    layoutMaxWidth: '990px',
+    layoutCommonPadding: '20px',
+
+    fontFamily: 'Helvetica, Arial, sans-serif',
+
+    layoutPrimaryBg: '#f5f5f5',
+    layoutSecondaryBg: '#fff',
+    layoutBorder: '#d8d8d8',
+    layoutBorderRadius: '2px',
+
     themePrimary: '#CC342D',
-    themeSecondary: '#585450'
+    themeSecondary: '#585450',
+
+    textColorMain: '#111',
+    textColorMuted: '#888'
 };
