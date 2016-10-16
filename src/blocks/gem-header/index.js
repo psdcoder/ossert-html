eacfb03a7ab8211058243cf2e26bec8d51113229
Pavel Grinchenko <psdcoder@gmail.com>
2016-10-16 18:03:15 +0300

    Adapt fixed header to new layout

--- src/blocks/gem-header/index.js
+++ src/blocks/gem-header/index.js
@@ -1,18 +1,13 @@
 const supportPageOffset = window.pageXOffset !== undefined;
 const isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
 const header = document.getElementById('sticky-project-header');
+const fixed = header.querySelector('.gem-header__fixed');
 
 export function onScroll() {
     const windowScroll = getWindowScroll();
-    const fix = header.offsetTop < windowScroll;
+    const fix = (header.offsetTop + header.offsetHeight + 15) < windowScroll;
 
-    if (fix) {
-        header.style.height = header.clientHeight + 'px';
-        header.classList.add('gem-header_sticky');
-    } else {
-        header.style.height = 'auto';
-        header.classList.remove('gem-header_sticky');
-    }
+    fixed.classList.toggle('gem-header__fixed_hidden', !fix);
 }
 
 function getWindowScroll() {
